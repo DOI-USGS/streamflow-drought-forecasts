6433712619f3df070c1a6dbdf8789605345bc529
Corson-Dosch <hcorson-dosch@usgs.gov>
2025-07-01 08:10:11 -0500

    switch to setup store

--- src/stores/global-data-store.js
+++ src/stores/global-data-store.js
@@ -1,14 +1,28 @@
 import { defineStore } from "pinia";
+// import { useRoute } from 'vue-router';
+import { computed, ref } from 'vue'; // Import ref for reactivity
 
-export const useGlobalDataStore = defineStore("globalDataStore", {
-  state: () => ({
-    dateInfoData: null,
-    selectedWeek: null,
-    siteInfoData: null,
-    selectedSite: null,
-  }),
-  getters: {
-    dataWeeks: (state) => state.dateInfoData.map(d => d.f_w) || null,
-    selectedDate: (state) => state.dateInfoData.find(d => d.f_w == state.selectedWeek).dt || null,
-  }
+export const useGlobalDataStore = defineStore("globalDataStore", () => {
+  const dateInfoData = ref(null)
+  const selectedWeek = ref(null)
+  const siteInfoData = ref(null)
+  const selectedSite = ref(null)
+  const defaultExtent = 'the continental U.S.'
+  const extents = [
+        "Alabama", "Arizona", "Arkansas", "California", "Colorado", 
+        "Connecticut", "Delaware", "Florida", "Georgia", "Idaho", 
+        "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", 
+        "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", 
+        "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", 
+        "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", 
+        "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", 
+        "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", 
+        "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", 
+        "Wyoming"
+    ]
+    const stateSelected = ref(false)
+    const dataWeeks = computed(() => dateInfoData.value.map(d => d.f_w) || null)
+    const selectedDate = computed(() => dateInfoData.value.find(d => d.f_w == selectedWeek.value).dt || null)
+
+  return { dateInfoData, selectedWeek, siteInfoData, selectedSite, defaultExtent, extents, stateSelected, dataWeeks, selectedDate}
 })
