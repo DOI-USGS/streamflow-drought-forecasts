94f0e720b60ab054cd35cc2bd80690495d872b75
Corson-Dosch <hcorson-dosch@usgs.gov>
2025-07-11 08:08:30 -0500

    Fix bug with log/linear toggle instantiation

--- src/components/TimeSeriesGraph.vue
+++ src/components/TimeSeriesGraph.vue
@@ -203,12 +203,12 @@
     {
       leftLabel: 'linear',
       rightLabel: 'log',
-      scaleLog: false,
+      scaleLog: scaleKind.value == 'log',
     }
   );
   // Watches toggleInfo for changes
   watch(toggleInfo, () => {
-    scaleKind.value = toggleInfo.scaleLog ? "log" : "linear";
+    scaleKind.value = toggleInfo.scaleLog ? 'log' : 'linear';
     siteHasChanged.value = false;
   });
 
