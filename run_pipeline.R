10c788b4a3b105d80b4deca1fb3c8e381ea74877
Jesse C. Ross <jross@usgs.gov>
2026-05-27 14:50:54 -0700

    fix errant parenthesis

--- run_pipeline.R
+++ run_pipeline.R
@@ -2,5 +2,5 @@ library(targets)
 
 # Run the pipeline
 cat("** Running the targets pipeline\n")
-cat(paste0("** RUNNING_ON_AWS_ECS is ", Sys.getenv("RUNNING_ON_AWS_ECS"), "\n")))
+cat(paste0("** RUNNING_ON_AWS_ECS is ", Sys.getenv("RUNNING_ON_AWS_ECS"), "\n"))
 targets::tar_make()
