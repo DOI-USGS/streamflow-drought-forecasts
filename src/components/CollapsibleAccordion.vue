6972cab28a3de4ab728c9f954700dd5d20775c64
Hayley Corson-Dosch <hcorson-dosch@usgs.gov>
2026-07-02 16:44:00 +0000

    Update FAQs for addition of watershed estimates and forecasts

--- src/components/CollapsibleAccordion.vue
+++ src/components/CollapsibleAccordion.vue
@@ -1,33 +1,37 @@
 <template>
-  <div 
+  <div
     :id="accordionId"
     class="accordion-container"
-    :style="{'border-left-color': leftBorderColor }"
-  > 
+    :style="{ 'border-left-color': leftBorderColor }"
+  >
     <h2>
       <button
-        :id="`${accordionId}-button`" 
-        class="accordion" 
+        :id="`${accordionId}-button`"
+        class="accordion"
         type="button"
         :class="{ active: active }"
         :aria-expanded="active ? true : false"
         :aria-controls="`${accordionId}-panel`"
-        :style="{'background-color': active ? buttonActiveBackgroundColor : buttonInactiveBackgroundColor, 'font-weight': buttonFontWeight, color: buttonFontColor}"
+        :style="{
+          'background-color': active ? buttonActiveBackgroundColor : buttonInactiveBackgroundColor,
+          'font-weight': buttonFontWeight,
+          color: buttonFontColor
+        }"
         @click="accordionClick"
       >
-        <p 
-          class="accordion-button-text" 
-          v-html="heading" 
+        <p
+          class="accordion-button-text"
+          v-html="heading"
         />
-        <span 
+        <span
           class="accordion-button-text symbol"
           :class="{ active: active }"
         >
-          <svg 
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
           >
-            <line 
+            <line
               class="symbol-line"
               x1="10"
               y1="1"
@@ -43,7 +47,7 @@
               y2="10"
               :stroke="buttonFontColor"
             />
-          </svg>  
+          </svg>
         </span>
       </button>
     </h2>
@@ -52,22 +56,20 @@
       role="region"
       class="panel"
       :aria-labelledby="`${accordionId}-button`"
-      :class="[{ 'active': active }]"
+      :class="[{ active: active }]"
     >
       <div
-        v-for="item, index in content"
+        v-for="(item, index) in content"
         :key="index"
       >
         <div
-          v-if="item.type=='text'"
+          v-if="item.type == 'text'"
           class="accordion-text-container"
         >
-          <p
-            v-html="item.content"
-          />
+          <p v-html="item.content" />
         </div>
         <div
-          v-if="item.type=='quote'"
+          v-if="item.type == 'quote'"
           class="accordion-text-container"
         >
           <p
@@ -76,61 +78,68 @@
           />
         </div>
         <div
-          v-if="item.type=='image'"
+          v-if="item.type == 'image'"
           class="accordion-image-container"
         >
           <img
             :src="getImageURL(item.content)"
             class="accordion-image"
-            :class="{ mobile: mobileView}"
+            :class="{ mobile: mobileView }"
           >
         </div>
         <div
-          v-if="item.type=='svg'"
+          v-if="item.type == 'svg'"
           class="accordion-svg-container"
           :style="{ width: item.width }"
           :class="item.class"
-        > 
-          <component 
-            :is="getSVG(item.content)" 
+        >
+          <component
+            :is="getSVG(item.content)"
             class="accordion-svg"
           />
         </div>
         <div
-          v-if="item.type=='flex'"
+          v-if="item.type == 'flex'"
           class="accordion-flex-container"
-          :style="{ 'flex-direction': item.flex_dir, 'align-items': item.flex_align, 'justify-content': item.flex_justify, 'gap': item.flex_gap, 'line-height': item.flex_line_height, 'margin': item.flex_margin}"
+          :style="{
+            'flex-direction': item.flex_dir,
+            'align-items': item.flex_align,
+            'justify-content': item.flex_justify,
+            gap: item.flex_gap,
+            'line-height': item.flex_line_height,
+            margin: item.flex_margin
+          }"
         >
           <div
-            v-for="flex_item, flex_index in item.content"
+            v-for="(flex_item, flex_index) in item.content"
             :key="flex_index"
           >
             <p
-              v-if="flex_item.type=='text'"
+              v-if="flex_item.type == 'text'"
               v-html="flex_item.content"
             />
             <p
-              v-if="flex_item.type=='quote'"
+              v-if="flex_item.type == 'quote'"
               class="quote"
               v-html="flex_item.content"
             />
             <div
-              v-if="flex_item.type=='image'"
+              v-if="flex_item.type == 'image'"
               class="accordion-image-container"
             >
               <img
                 :src="getImageURL(flex_item.content)"
                 class="accordion-image"
-                :class="{ mobile: mobileView}"
+                :class="{ mobile: mobileView }"
               >
             </div>
             <div
-              v-if="flex_item.type=='svg'"
+              v-if="flex_item.type == 'svg'"
               :style="{ width: flex_item.width, height: flex_item.height }"
               :class="flex_item.class"
-            > 
-              <component 
-                :is="getSVG(flex_item.content)" 
+            >
+              <component
+                :is="getSVG(flex_item.content)"
                 class="accordion-svg"
                 aria-hidden="true"
               />
@@ -143,93 +152,93 @@
 </template>
 
 <script setup>
-  import { defineAsyncComponent, nextTick, ref, watch } from "vue";
-  import { isMobile } from 'mobile-device-detect';
+import { defineAsyncComponent, nextTick, ref, watch } from 'vue'
+import { isMobile } from 'mobile-device-detect'
 
-  const props = defineProps({
-    activeOnLoad: Boolean, // should accordion be open on load
-    accordionId: {
-      type: String,
-      required: true,
-      default: ""
-    }, // id of acordion, for selection
-    heading: {
-      type: String,
-      required: true,
-      default: 'Accordion heading'
-    },
-    content: {
-      type: Array,
-      required: true,
-      default: () => ([
-        {
-          type: 'text',
-          content: 'Accordion content'
-        }
-      ]),
-    },
-    leftBorderColor: {
-      type: String,
-      required: true,
-      default: '#000000'
-    },
-    buttonActiveBackgroundColor: {
-      type: String,
-      required: true,
-      default: '#F2F2F2'
-    },
-    buttonInactiveBackgroundColor: {
-      type: String,
-      required: true,
-      default: '#ffffff'
-    },
-    buttonFontWeight: {
-      type: String,
-      required: true,
-      default: '800'
-    },
-    buttonFontColor: {
-      type: String,
-      required: true,
-      default: '#000000'
-    },
-    tooltipFunction: {
-      type: Function,
-      required: false,
-      default: () => {
-        console.log('Default function');
+const props = defineProps({
+  activeOnLoad: Boolean, // should accordion be open on load
+  accordionId: {
+    type: String,
+    required: true,
+    default: ''
+  }, // id of acordion, for selection
+  heading: {
+    type: String,
+    required: true,
+    default: 'Accordion heading'
+  },
+  content: {
+    type: Array,
+    required: true,
+    default: () => [
+      {
+        type: 'text',
+        content: 'Accordion content'
       }
+    ]
+  },
+  leftBorderColor: {
+    type: String,
+    required: true,
+    default: '#000000'
+  },
+  buttonActiveBackgroundColor: {
+    type: String,
+    required: true,
+    default: '#F2F2F2'
+  },
+  buttonInactiveBackgroundColor: {
+    type: String,
+    required: true,
+    default: '#ffffff'
+  },
+  buttonFontWeight: {
+    type: String,
+    required: true,
+    default: '800'
+  },
+  buttonFontColor: {
+    type: String,
+    required: true,
+    default: '#000000'
+  },
+  tooltipFunction: {
+    type: Function,
+    required: false,
+    default: () => {
+      console.log('Default function')
     }
-  })
+  }
+})
 
-  // global variables
-  const mobileView = isMobile;
-  const active = ref(props.activeOnLoad);
+// global variables
+const mobileView = isMobile
+const active = ref(props.activeOnLoad)
 
-  watch(active, () => {
-    if (active.value == true) {
-      if (props.tooltipFunction) {
-        handleTooltips()
-      }
+watch(active, () => {
+  if (active.value == true) {
+    if (props.tooltipFunction) {
+      handleTooltips()
     }
-  });
-
-  async function handleTooltips() {
-    await nextTick();
-    props.tooltipFunction(props.accordionId)
   }
+})
 
-  function accordionClick() {
-    active.value = !active.value;
-  }
+async function handleTooltips() {
+  await nextTick()
+  props.tooltipFunction(props.accordionId)
+}
 
-  function getImageURL(file) {
-    return new URL(`../assets/images/${file}`, import.meta.url).href
-  }
+function accordionClick() {
+  active.value = !active.value
+}
 
-  function getSVG(file) {
-    return defineAsyncComponent(() => import(`../../src/assets/svgs/${file}.svg`));
-  }
+function getImageURL(file) {
+  return new URL(`../assets/images/${file}`, import.meta.url).href
+}
+
+function getSVG(file) {
+  return defineAsyncComponent(() => import(`../../src/assets/svgs/${file}.svg`))
+}
 </script>
 
 <style scoped lang="scss">
@@ -240,13 +249,14 @@ $padding: 10px;
 $text-padding: 0.5rem;
 $left-border-width: 5px;
 .accordion-container {
-  border-left: $left-border-width solid;  
+  border-left: $left-border-width solid;
   border-right: 1px solid var(--very-light-grey);
   border-top: 1px solid var(--very-light-grey);
   border-bottom: 1px solid var(--very-light-grey);
-  border-radius: .25rem;
+  border-radius: 0.25rem;
   overflow-wrap: break-word;
-  margin: $margin calc(($margin + $left-border-width/2)*-1) $margin calc(($margin + $left-border-width/2)*-1);
+  margin: $margin calc(($margin + $left-border-width/2) * -1) $margin
+    calc(($margin + $left-border-width/2) * -1);
   overflow: hidden;
   box-shadow: 0px 0px 3px 1.5px rgba(0, 0, 0, 0.05);
 }
@@ -269,11 +279,11 @@ $left-border-width: 5px;
   gap: 5px;
   align-items: center;
   position: relative;
-  border-radius: 0 .25rem .25rem 0;
+  border-radius: 0 0.25rem 0.25rem 0;
 }
 .accordion:focus-visible {
   border: 2px solid;
-  border-radius: 0 .25rem .25rem 0;
+  border-radius: 0 0.25rem 0.25rem 0;
 }
 .accordion:hover {
   background-color: var(--medium-light-grey) !important;
@@ -298,11 +308,11 @@ $left-border-width: 5px;
   stroke-width: 0.8px;
 }
 .panel {
-  display: none; 
+  display: none;
   margin: 2rem 0 2rem 0;
 }
 .panel.active {
-  display: block; 
+  display: block;
 }
 .accordion-text-container {
   padding: 0 $padding 0 0;
@@ -313,18 +323,10 @@ $left-border-width: 5px;
 }
 .panel p {
   padding: 0 0 $text-padding 0;
-  font-weight: 400 !important;  
-}
-.accordion-image-container {
-  text-align: center;
+  font-weight: 400 !important;
 }
 .accordion-image {
-  padding: 10px;
-  max-width: 95%;
-}
-.accordion-image.mobile {
-  padding: 10px;
-  max-width: 100%;
+  max-width: 90%;
 }
 .accordion-svg-container {
   padding: 10px;
